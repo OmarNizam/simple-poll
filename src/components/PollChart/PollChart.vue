44e70d0ff188a987a493228b2e4b1360723086b8
OmarNizam <eng.omarnizam@gmail.com>
2022-11-28 19:06:20 +0100

    implementing pinia store and get state in PollChart component

--- src/components/PollChart/PollChart.vue
+++ src/components/PollChart/PollChart.vue
@@ -7,66 +7,54 @@
 </template>
 
 <script>
+import { mapState } from "pinia";
+import { useApplicationStore } from "@/stores/ApplicationStore";
 import { Chart, registerables } from "chart.js";
 
 Chart.register(...registerables);
 
 export default {
   name: "PollChart",
-  data() {
-    return {
-      xValues: ["Italy", "France", "Spain", "USA", "Argentina", "Syria"],
-      yValues: [44, 25, 32, 19, 29, 9],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-        "rgba(255, 205, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(201, 203, 207, 0.2)",
-      ],
-      borderColor: [
-        "rgb(255, 99, 132)",
-        "rgb(255, 159, 64)",
-        "rgb(255, 205, 86)",
-        "rgb(75, 192, 192)",
-        "rgb(54, 162, 235)",
-        "rgb(153, 102, 255)",
-        "rgb(201, 203, 207)",
-      ],
-      borderWidth: 1,
-    };
+  computed: {
+    ...mapState(useApplicationStore, ["chartState"]),
   },
   async mounted() {
-    const ctx = document.getElementById("poll-chart");
-    new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: this.xValues,
-        datasets: [
-          {
-            label: "Votes count",
-            backgroundColor: this.backgroundColor,
-            borderColor: this.borderColor,
-            borderWidth: this.borderWidth,
-            data: this.yValues,
-          },
-        ],
-      },
-      options: {
-        legend: { display: false },
-        title: {
-          display: true,
-          text: "Simple poll chart bar 2022",
+    this.createChart();
+  },
+  methods: {
+    /**
+     * createChart method create new Chart getting data from chartState
+     */
+    createChart() {
+      const ctx = document.getElementById("poll-chart");
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: this.chartState.xValues,
+          datasets: [
+            {
+              label: "Votes count",
+              backgroundColor: this.chartState.backgroundColor,
+              borderColor: this.chartState.borderColor,
+              borderWidth: this.chartState.borderWidth,
+              data: this.chartState.yValues,
+            },
+          ],
         },
-        scales: {
-          y: {
-            beginAtZero: true,
+        options: {
+          legend: { display: false },
+          title: {
+            display: true,
+            text: "Simple poll chart bar 2022",
+          },
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
           },
         },
-      },
-    });
+      });
+    },
   },
 };
 </script>
