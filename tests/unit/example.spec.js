55a2ebabbc1d20be0553f5a35c8c6c6164eb7fa4
OmarNizam <eng.omarnizam@gmail.com>
2022-12-05 07:47:48 +0100

    fix unit test set up

--- tests/unit/example.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld/HelloWorld.vue";
-
-describe("HelloWorld.vue", () => {
-  it("Load component", () => {
-    const wrapper = shallowMount(HelloWorld);
-    expect(wrapper.exists()).toBeTruthy();
-  });
-});
